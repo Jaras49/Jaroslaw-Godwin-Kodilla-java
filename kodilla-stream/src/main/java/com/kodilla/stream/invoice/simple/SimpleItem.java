0b005413df9669ce7998be196b35220e2c6a9246
Jaras49 <jaras48@gmail.com>
2018-03-23 16:19:01 +0100

    BigDecimal

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/invoice/simple/SimpleItem.java
@@ -0,0 +1,24 @@
+package com.kodilla.stream.invoice.simple;
+
+public final class SimpleItem {
+
+    private final SimpleProduct product;
+    private final double quantity;
+
+    public SimpleItem(SimpleProduct simpleProduct, double quantity) {
+        this.product = simpleProduct;
+        this.quantity = quantity;
+    }
+
+    public SimpleProduct getSimpleProduct() {
+        return product;
+    }
+
+    public double getQuantity() {
+        return quantity;
+    }
+
+    public double getValue(){
+        return product.getProductPrice() * quantity;
+    }
+}
