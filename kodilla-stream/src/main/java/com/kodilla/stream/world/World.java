e97143798feace4dfb9f82ea0509984362338602
Jaras49 <jaras48@gmail.com>
2018-03-24 21:02:25 +0100

    [7.4] word population

--- kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -8,31 +8,9 @@ public final class World {
 
     private final List<Continent> continents;
 
-    public World(){
+    public World(List<Continent> continents){
 
-        List<Country> countriesEurope = new ArrayList<>();
-        countriesEurope.add(new Country("England", new BigDecimal("53000000")));
-        countriesEurope.add(new Country("Poland", new BigDecimal("30000000")));
-        countriesEurope.add(new Country("Germany", new BigDecimal("40000000")));
-
-        List<Country> countriesAfrica = new ArrayList<>();
-        countriesAfrica.add(new Country("Egipt", new BigDecimal("35000000")));
-        countriesAfrica.add(new Country("Marocco", new BigDecimal("20000000")));
-        countriesAfrica.add(new Country("Algeria", new BigDecimal("25000000")));
-
-        List<Country> countriesAmerica = new ArrayList<>();
-        countriesAfrica.add(new Country("USA", new BigDecimal("100000000")));
-        countriesAfrica.add(new Country("Mexico", new BigDecimal("15000000")));
-        countriesAfrica.add(new Country("Canada",new BigDecimal("40000000")));
-
-        Continent europe = new Continent("Europe", countriesEurope);
-        Continent africa = new Continent("Africa", countriesAfrica);
-        Continent america = new Continent("america", countriesAmerica);
-
-        continents = new ArrayList<>();
-        continents.add(europe);
-        continents.add(africa);
-        continents.add(america);
+        this.continents = continents;
     }
 
     public BigDecimal getPeopleQuantity(){
--- kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/world/WorldTestSuite.java
@@ -5,6 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WorldTestSuite {
 
@@ -13,7 +15,31 @@ public class WorldTestSuite {
     @Before
     public void setUp() throws Exception {
 
-        world = new World();
+        List<Country> countriesEurope = new ArrayList<>();
+        countriesEurope.add(new Country("England", new BigDecimal("53000000")));
+        countriesEurope.add(new Country("Poland", new BigDecimal("30000000")));
+        countriesEurope.add(new Country("Germany", new BigDecimal("40000000")));
+
+        List<Country> countriesAfrica = new ArrayList<>();
+        countriesAfrica.add(new Country("Egipt", new BigDecimal("35000000")));
+        countriesAfrica.add(new Country("Marocco", new BigDecimal("20000000")));
+        countriesAfrica.add(new Country("Algeria", new BigDecimal("25000000")));
+
+        List<Country> countriesAmerica = new ArrayList<>();
+        countriesAfrica.add(new Country("USA", new BigDecimal("100000000")));
+        countriesAfrica.add(new Country("Mexico", new BigDecimal("15000000")));
+        countriesAfrica.add(new Country("Canada",new BigDecimal("40000000")));
+
+        Continent europe = new Continent("Europe", countriesEurope);
+        Continent africa = new Continent("Africa", countriesAfrica);
+        Continent america = new Continent("america", countriesAmerica);
+
+        List<Continent> continents = new ArrayList<>();
+        continents.add(europe);
+        continents.add(africa);
+        continents.add(america);
+
+        world = new World(continents);
     }
 
     @Test
@@ -21,10 +47,11 @@ public class WorldTestSuite {
 
         //Given
         BigDecimal expectedWorldPeopleQuantity = new BigDecimal("358000000");
+
         //when
         BigDecimal worldPeopleQuantity = world.getPeopleQuantity();
+
         //Then
         Assert.assertEquals(expectedWorldPeopleQuantity, worldPeopleQuantity);
-
     }
 }
