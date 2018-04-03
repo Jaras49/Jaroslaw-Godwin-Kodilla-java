e751d993cea54368e38abd6013b4d46bc91efed3
Jaras49 <jaras48@gmail.com>
2018-04-03 20:56:51 +0200

    [9.1] MovieStore

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Main.java
@@ -0,0 +1,17 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MovieStore movieStore = new MovieStore();
+
+        String collect = movieStore.getMovies().entrySet().stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .collect(Collectors.joining("!"));
+
+        System.out.println(collect);
+    }
+}
