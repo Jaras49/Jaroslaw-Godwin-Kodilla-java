e9f094beb2d9d5fcf72398853cb5300ca8094143
Jaras49 <jaras48@gmail.com>
2018-04-10 20:05:59 +0200

    [19.1] Bean ForumUser

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,29 @@
+package com.kodilla.spring.forum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ForumUserTestSuite {
+
+    @Test
+    public void testGetUserName() {
+
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.forum");
+        ForumUser user = context.getBean(ForumUser.class);
+
+        //When
+        String userName = user.getUserName();
+
+        //Then
+        assertEquals("John Smith", userName);
+    }
+}
