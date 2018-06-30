a58b5d2895cd396015982afbdd0fcd05f0e32d79
Jaras49 <jaras48@gmail.com>
2018-06-30 19:03:59 +0200

    [20.3] OrderFacade aspect

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/aop/calculator/Watcher.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/aop/calculator/Watcher.java
@@ -1,6 +1,7 @@
 package com.kodilla.patterns2.aop.calculator;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -17,7 +18,7 @@ public class Watcher {
     private static final Logger LOGGER = LoggerFactory.getLogger(Watcher.class);
 
     @Before("execution(* com.kodilla.patterns2.aop.calculator.Calculator.factorial(..))" +
-    "&& args(theNumber) && target(object)")
+            "&& args(theNumber) && target(object)")
     public void logEvent(BigDecimal theNumber, Object object) {
         LOGGER.info("Class: " + object.getClass().getName() + ", Args: " + theNumber);
     }
@@ -31,6 +32,27 @@ public class Watcher {
             result = proceedingJoinPoint.proceed();
             long end = System.currentTimeMillis();
             LOGGER.info("Time consumed " + (end - begin) + " [m/s]");
+        } catch (Throwable throwable) {
+            LOGGER.error(throwable.getMessage());
+            throw throwable;
+        }
+        return result;
+    }
+
+    @After("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public void logProcessOrderExecution() {
+        LOGGER.info("Order processed");
+    }
+
+    @Around("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public Object measureProcessOrderTime(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result;
+
+        try {
+            long begin = System.currentTimeMillis();
+            result = proceedingJoinPoint.proceed();
+            long end = System.currentTimeMillis();
+            LOGGER.info("ProcessOrder time consumed " + (end - begin) + " [m/s]");
         } catch (Throwable throwable) {
             LOGGER.error(throwable.getMessage());
             throw throwable;
